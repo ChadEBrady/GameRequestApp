8ab5f599569709af047899364a83606f6146e8bf
Chad Brady <chad.brady@buzzfeed.com>
2022-07-30 19:22:06 -0500

    Initial XCode Commit

---
+++ GameSearchApp/GameSearchApp/GameSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameSearchAppApp.swift
+//  GameSearchApp
+//
+//  Created by Chad Brady on 7/30/22.
+//
+
+import SwiftUI
+
+@main
+struct GameSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
